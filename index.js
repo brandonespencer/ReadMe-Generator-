187d21d55370645c34b329149d0c8e6ede0e66f8
Brandon Spencer <46231696+brandonespencer@users.noreply.github.com>
2022-02-12 02:15:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,128 +1,107 @@
-// External packages
 const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
-
-// Internal modules
-const api = require('./utils/api.js');
+// const generatePage = require('./src/page-template.js');
 const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
-// Inquirer prompts for userResponses
 const questions = [
     {
         type: 'input',
-        message: "What is your GitHub username? (No @ needed)",
-        name: 'username',
-        default: 'brandonespencer',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid GitHub username is required.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: "What is the name of your GitHub repo?",
-        name: 'repo',
-        default: 'readme-generator',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid GitHub repo is required for a badge.");
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        message: "What is the title of your project?",
         name: 'title',
-        default: 'Project Title',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid project title is required.");
+        message: 'What is the title of your app? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your app name!');
+                return false;
             }
-            return true;
         }
     },
     {
         type: 'input',
-        message: "Write a description of your project.",
+        name: 'gitHubName',
+        message: 'Provide github username (Required)',
+        validate: gitHubInput => {
+            if (gitHubInput) {
+                return true;
+            } else {
+                console.log('Please describe your username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
         name: 'description',
-        default: 'Project Description',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("A valid project description is required.");
+        message: 'Provide a description of the app (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please describe your app!');
+                return false;
             }
-            return true;
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did languages did you write this project with? (Check all that apply)',
+        choices: [' JavaScript ', ' HTML ', ' CSS ', ' ES6 ', ' jQuery ', ' Bootstrap ', ' Node ']
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install this app (Required)',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please describe installation steps!');
+                return false;
+            }
         }
     },
     {
         type: 'input',
-        message: "If applicable, describe the steps required to install your project for the Installation section.",
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: "Provide instructions and examples of your project in use for the Usage section.",
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
-        name: 'contributing'
-    },
-    {
-        type: 'input',
-        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
-        name: 'tests'
-    },
-    {
-        type: 'list',
-        message: "Choose a license for your project.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license'
-    }
-];
+        name: 'collaborators',
+        message: 'Did you have any collaborators on this app? If no enter "none"'
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if (err) {
-          return console.log(err);
-        }
-      
-        console.log("Success! Your README.md file has been generated")
-    });
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license will you use? If you will not use a license enter "none"'
+        // choices: [' MIT ', ' HTML ', ' CSS ', ' ES6 ', ' jQuery ', ' Bootstrap ', ' Node ']
+        
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email to handle and questions regarding your app: '
+        // choices: [' MIT ', ' HTML ', ' CSS ', ' ES6 ', ' jQuery ', ' Bootstrap ', ' Node ']
+        
+    },
+
+]
+
+// function to write README file
+// function writeToFile(fileName, data) {
+// fs.writeFile
+// }
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        fs.writeFile("README.md", generateMarkdown(response), err => {
+            if (err) {
+                throw err;
+            }
+
+        })
+    })
+
 }
 
-const writeFileAsync = util.promisify(writeToFile);
-
-
-// Main function
-async function init() {
-    try {
-
-        // Prompt Inquirer questions
-        const userResponses = await inquirer.prompt(questions);
-        console.log("Your responses: ", userResponses);
-        console.log("Thank you for your responses! Fetching your GitHub data next...");
-    
-        // Call GitHub api for user info
-        const userInfo = await api.getUser(userResponses);
-        console.log("Your GitHub user info: ", userInfo);
-    
-        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
-        console.log("Generating your README next...")
-        const markdown = generateMarkdown(userResponses, userInfo);
-        console.log(markdown);
-    
-        // Write markdown to file
-        await writeFileAsync('ExampleREADME.md', markdown);
-
-    } catch (error) {
-        console.log(error);
-    }
-};
-
 init();
-
+    //     fs.appendFileSync("README.md", ("##" + response.title + '\n') + '\n', function (err) {
